42f6a7da9fc81165c9c6d97fdf6e7875ee3dc9e3
nicksoule <nicksoule@live.com>
2018-01-23 14:16:59 -0500

    first commit

---
+++ src/NewGitDemo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class NewGitDemo {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String userName;
+		
+		userName = scan.nextLine();
+		
+		System.out.println(userName);
+	
+
+	}
+
+}
